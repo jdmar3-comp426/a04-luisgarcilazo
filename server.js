77b6f8afe75f9b6a38c7025f5ae80754f2a7ba27
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2021-10-30 22:44:30 -0400

    checking

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 // Define app using express
-var express = require("express")
-var app = express()
+var express = require("express");
+var app = express();
 // Require database SCRIPT file
+var db = require("./database.js");
 
 // Require md5 MODULE
-
+var md5 = require('md5');
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set server port
-
+let HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
@@ -23,6 +24,13 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
+app.post("/app/new", (req,res) => {
+	const stmt = db.prepare("INSERT INTO userInfo (user, pass) VALUES (?,?)");
+	const info = stmt.run(req.body.user, req.body.pass)
+	if (info.changes > 0){
+		res.status(201).json({"message": "Created (201)"});
+	} 
+});
 
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
 app.get("/app/users", (req, res) => {	
@@ -38,6 +46,6 @@ app.get("/app/users", (req, res) => {
 
 // Default response for any other request
 app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
